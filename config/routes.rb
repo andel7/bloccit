27fa5254abde5a1c89320b3fdab6f2db5f1e5808
andel7 <lev.andelman@gmail.com>
2014-11-10 22:45:10 +0200

    added posts index and post show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+ 
+  resources :posts
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
   root to: 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
