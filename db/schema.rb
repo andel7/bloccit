53bf1b0f1911076137d563938ec1af5fcee58279
andel7 <lev.andelman@gmail.com>
2014-11-18 18:03:46 +0200

    added associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115105330) do
+ActiveRecord::Schema.define(version: 20141118154015) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 20141115105330) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -44,8 +47,7 @@ ActiveRecord::Schema.define(version: 20141115105330) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email" # Only if using reconfirmable
-
+    t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
