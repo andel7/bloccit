adef4f2b9ed1cf4d7361ebda3c4d7af99e56d183
andel7 <lev.andelman@gmail.com>
2014-11-30 21:35:00 +0200

    added avatar to the profile 2

---
+++ config/initializers/fog.rb
@@ -0,0 +1,12 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:               'AWS',
+  #  aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+  #  aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY']
+    aws_access_key_id:      'AKIAJJOQZ7PYCICDIHPQ',
+    aws_secret_access_key:  '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',
+    region: 'us-east-1'
+  }
+  config.fog_directory  = 'andel7-bloccit-development'
+  config.fog_public     = true
+end
