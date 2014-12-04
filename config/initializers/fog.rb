51c9b19df2b63a229ea9426378517af434d4815c
andel7 <lev.andelman@gmail.com>
2014-12-04 17:59:42 +0200

    added some stuff

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,12 +1,13 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
-  #  aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
-  #  aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY']
-    aws_access_key_id:      'AKIAJJOQZ7PYCICDIHPQ',
-    aws_secret_access_key:  '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',
+    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+  #  aws_access_key_id:      'AKIAJJOQZ7PYCICDIHPQ',
+  #  aws_secret_access_key:  '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',
     region: 'us-east-1'
   }
-  config.fog_directory  = 'andel7-bloccit-development'
+  config.fog_directory  = ENV['AWS_BUCKET']
+  #config.fog_directory  = 'andel7-bloccit-development'
   config.fog_public     = true
 end
