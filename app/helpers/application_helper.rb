4f664d4b49a7ae6f57485234cedd55dd8ec2f209
andel7 <lev.andelman@gmail.com>
2015-02-07 16:59:15 +0200

    refactoring upvotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,21 @@
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render text).html_safe
   end
+
+  def vote_link_classes(post,direction)
+    if direction == "up"
+        if (current_user.voted(post) && current_user.voted(post).up_vote?)
+          return 'glyphicon glyphicon-chevron-up voted'
+        else
+          return 'glyphicon glyphicon-chevron-up'
+        end
+        
+    elsif direction == "down"
+        if (current_user.voted(post) && current_user.voted(post).down_vote?)
+          return 'glyphicon glyphicon-chevron-down voted' 
+       else
+          return 'glyphicon glyphicon-chevron-down' 
+        end
+    end
+  end
  end
